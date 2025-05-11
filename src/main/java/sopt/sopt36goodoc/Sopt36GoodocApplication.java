a9f19a8a95d50cdc221ca758d8e0d4c9cd432ef7
jher235 <tim668666@gmail.com>
2025-05-11 19:06:25 +0900

    Setting: 스프링 프로젝트 세팅 - #1

---
+++ src/main/java/sopt/sopt36goodoc/Sopt36GoodocApplication.java
@@ -0,0 +1,13 @@
+package sopt.sopt36goodoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sopt36GoodocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sopt36GoodocApplication.class, args);
+    }
+
+}
