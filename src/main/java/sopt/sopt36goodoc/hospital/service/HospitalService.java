2ff9413aa9c9e379039ad3d6a605235e7b33fc0b
ssangbaam <tkdqja2196@naver.com>
2025-05-13 21:26:40 +0900

    Feature: 병원 목록 조회를 위한 Service 생성 - #5

---
+++ src/main/java/sopt/sopt36goodoc/hospital/service/HospitalService.java
@@ -0,0 +1,30 @@
+package sopt.sopt36goodoc.hospital.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sopt.sopt36goodoc.hospital.domain.Hospital;
+import sopt.sopt36goodoc.hospital.dto.response.HospitalListResponse;
+import sopt.sopt36goodoc.hospital.dto.response.HospitalResponse;
+import sopt.sopt36goodoc.hospital.repository.HospitalRepository;
+
+import java.time.LocalTime;
+
+@Service
+@RequiredArgsConstructor
+public class HospitalService {
+    private final HospitalRepository hospitalRepository;
+
+    public HospitalListResponse getHospitals() {
+        return HospitalListResponse.from(
+                hospitalRepository.findAll()
+                        .stream()
+                        .map(hospital -> HospitalResponse.of(hospital, isOpen(hospital)))
+                        .toList()
+                );
+    }
+
+    private Boolean isOpen(Hospital hospital) {
+        LocalTime now = LocalTime.now();
+        return now.isAfter(hospital.getOpenAt()) && now.isBefore(hospital.getCloseAt());
+    }
+}
